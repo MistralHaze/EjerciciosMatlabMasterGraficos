72dec0a9324bbdb332966af8ec53b5bd81d7aab5
Carlos Rizo <carima96@gmail.com>
2019-11-25 21:24:33 +0100

    Finished the calculus part of the second exercise. Graphics of the second exercise and point b) of both exercises are left

--- EulerExplicito.m
+++ EulerExplicito.m
@@ -1,6 +1,8 @@
 function EulerExplicito()
-Ej0();
+% Ej0();
+Ej1();
 end
+
 
 function Ej0()
 h=0.2;
@@ -22,15 +24,55 @@ while (tn<t_end)
     n = n+1;
     
 end
+
 % We now calculate the real y(t)
 real_y=0:0.2:1;
 func_real_y=(real_y+1).^2 -0.5*exp(real_y);
 error= abs(y - func_real_y);
-
 fprintf('Euler %f Real %f Error %f \n', y ,func_real_y, error);
 
 plot(0:h:t_end,y, real_y,func_real_y);
+end
 
+
+function Ej1()
+h=0.05;
+tn=0.0;
+t_end=0.1;
+
+m=10;
+B=50;
+k=200;
+
+N =calculateNumberOfSteps(tn, t_end, h);
+
+u= 1:N;
+u(1)=0;
+
+v= 1:N;
+v(1)=1;
+
+n=1;
+while (tn<t_end)
+%     Calculamos primero para u con el anterior valor de v
+    f_tnun= (-B * abs(v(n)) * v(n) -k*u(n))/m;
+    
+    u(n+1)=u(n) + h* v(n);
+    v(n+1)=v(n) + h *f_tnun;
+    
+    tn = tn + h;
+    n = n+1;
+    
+end
+% Draw the plot with the calculated values
+% func_u= M*
+
+
+for i=1:3
+fprintf('U%i %f ',i, u(i));
+fprintf('V%i %f ',i, v(i));
+fprintf('\n')
+end
 
 end
 
